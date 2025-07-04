cbed62039190d812737ea08b8f51fe52031d57a8
Srijita Naha Biswas <nahabiswassrijita@gmail.com>
2025-07-04 13:14:42 +0530

    Add files via upload

---
+++ 1/Library Management/src/main/java/com/Library/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+import com.library.repository.BookRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = context.getBean("bookService", BookService.class);
+        BookRepository bookRepository = context.getBean("bookRepository", BookRepository.class);
+
+        System.out.println("Spring context loaded successfully.");
+    }
+}
